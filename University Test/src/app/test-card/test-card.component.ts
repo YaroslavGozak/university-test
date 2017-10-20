4e65e1b11389927d0907933fe64ac4e00f031ebe
Yuliia Dehtiarova <yulidehtiarova@gmail.com>
2017-10-21 00:37:27 +0300

    +Timer

--- University Test/src/app/test-card/test-card.component.ts
+++ University Test/src/app/test-card/test-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Test, Question, Answer} from '../models/main-models';
 import { HttpModule }           from '@angular/http';
 import { TestsService } from '../services/tests.service';
+import { Observable, Subscription } from 'rxjs/Rx';
 @Component({
   selector: 'app-test-card',
   templateUrl: './test-card.component.html',
@@ -9,6 +10,14 @@ import { TestsService } from '../services/tests.service';
 	providers:[HttpModule]
 })
 export class TestCardComponent implements OnInit{
+	 ticks = 0;
+    
+    minutesDisplay: number = 0;
+    hoursDisplay: number = 0;
+    secondsDisplay: number = 0;
+
+    sub: Subscription;
+
 private tests:Test[];
 private results:number[];
 private selectedTest:Test;
@@ -19,7 +28,37 @@ constructor(private testService: TestsService){}
 ngOnInit()
 {
 	this.testService.getAllTests().then(data=>this.tests=data);
+	this.startTimer();
 }
+  private startTimer() {
+
+        let timer = Observable.timer(1, 1000);
+        this.sub = timer.subscribe(
+            t => {
+                this.ticks = t;
+                
+                this.secondsDisplay = this.getSeconds(this.ticks);
+                this.minutesDisplay = this.getMinutes(this.ticks);
+                this.hoursDisplay = this.getHours(this.ticks);
+            }
+        );
+    }
+ private getSeconds(ticks: number) {
+        return this.pad(ticks % 60);
+    }
+
+    private getMinutes(ticks: number) {
+         return this.pad((Math.floor(ticks / 60)) % 60);
+    }
+
+    private getHours(ticks: number) {
+        return this.pad(Math.floor((ticks / 60) / 60));
+    }
+
+    private pad(digit: any) { 
+        return digit <= 9 ? '0' + digit : digit;
+    }
+
 selectTest(test:Test)
 {
 	this.currentQuestion=0;
