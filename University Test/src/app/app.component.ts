c96b64248fc9e1cf42996a359859da5e301fd44c
RomanPopovAndrsen <r.popov@andersenlab.com>
2017-10-27 19:02:50 +0300

    Rewritten to bootstrap

--- University Test/src/app/app.component.ts
+++ University Test/src/app/app.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {Test, Question, Answer} from './models/main-models';
+import { Test, Question, Answer} from './models/main-models';
 import { HttpModule }           from '@angular/http';
 import { TestsService } from './services/tests.service';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-	private tests:Test[];
-private results:number[];
-private selectedTest:Test;
-private currentQuestion:number;
-constructor(private testService: TestsService){}
-//this.items = this.testService.getData();
-ngOnInit()
-{
-	this.testService.getAllTests().then(data=>this.tests=data);
-}
-	selectTest(test:Test)
-{
-	
-	this.currentQuestion=0;
-	this.selectedTest=test;
-}
+export class AppComponent implements OnInit {
+    private tests: Test[];
+    private results: number[];
+    private selectedTest: Test;
+    private currentQuestion: number;
+    constructor(private testService: TestsService,
+        private router: Router) { }
+    //this.items = this.testService.getData();
+    ngOnInit() {
+        this.testService.getAllTests().then(data => this.tests = data);
+    }
+    selectTest(test: Test) {
+
+        this.currentQuestion = 0;
+        this.selectedTest = test;
+        this.router.navigate(['/test', test.testID]);
+    }
 }
--- University Test/src/app/app.module.ts
+++ University Test/src/app/app.module.ts
@@ -1,35 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { TestCardComponent } from './test-card/test-card.component';
-import {MatCardModule,MatRadioModule,MatButtonModule} from '@angular/material';
+//import {MatCardModule,MatRadioModule,MatButtonModule} from '@angular/material';
 import { AppComponent } from './app.component';
 //import { HttpClientModule } from '@angular/common/http';
-import {HttpModule} from '@angular/http';
+import { HttpModule} from '@angular/http';
 import { TestsService } from './services/tests.service';
+import { PopupModule } from 'ng2-opd-popup';
 const appRoutes: Routes = [
+    {
+        path: 'test/:id',
+        component: TestCardComponent,
+    },
   {
-    path: 'test/:id',
-    component: TestCardComponent,
+      path: '**',
+      redirectTo: ''
   }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestCardComponent
-  ],
-  imports: [ 
-	     RouterModule.forRoot(
-      appRoutes
-    ),
-    BrowserModule,	
-	MatCardModule,
-	MatRadioModule,
-	MatButtonModule
-  ],
-  providers: [TestsService, HttpModule],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TestCardComponent
+    ],
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        ),
+        BrowserModule,
+        PopupModule.forRoot()
+    ],
+    providers: [TestsService, HttpModule],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
